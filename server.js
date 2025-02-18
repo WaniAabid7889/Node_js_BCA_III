9f2354fd4534c7d3e81c83f39b1586cbbcd3958e
abidhussain <waniabid645@gmail.com>
2025-02-18 10:19:15 +0530

    put and delete method with express js

--- server.js
+++ server.js
@@ -1,84 +1,22 @@
 const express = require('express');
 const port = 3004;
 const app = express();
-
-
 app.use(express.json());
-
 app.get('/',(req,res)=>{
   res.send("welcome to express js ");
 })
 
-
-// app.get('/users',(req,res)=>{
-//   res.json({result : {id:'101',name:'user1'}});
-// })
-
-
-app.get('/users',async(req,res)=>{
-  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  // console.log(response);
-  let data = await response.json();
-  res.json({result : data});
-})
-
-let users =[
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    },
-    {
-      "userId": 1,
-      "id": 4,
-      "title": "eum et est occaecati",
-      "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-    },
-    {
-      "userId": 1,
-      "id": 5,
-      "title": "nesciunt quas odio",
-      "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-    }
-];
-
-app.get('/users/:id',(req,res)=>{
-    let  id = req.params.id;
+app.put('/update/:id?',(req,res)=>{
+    let id = req.params.id;
+    let data = req.body;
     console.log(id);
-    if(users[0].id==id){
-      console.log(users[0].id);
-      res.json({data : users[0]});
-    }
+    console.log(data);
+    res.json({success:true,result: data, message:"Record is updated"});
 })
 
-
-app.post('/insert',(req,res)=>{
-  let result = req.body;
-  res.json({message : result}); 
+app.delete('/delete/:id?',(req,res)=>{
+   res.status(200).json({success:true, message:"record is deleted"});
 })
-
-
-app.get('/query',(req,res)=>{
-  let queryString = req.query
-  console.log(queryString);
-  res.json({urlData : queryString});
-})
-
-
-
 
 
 app.listen(port);
