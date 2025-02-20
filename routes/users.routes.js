72c536af37a222b751d88d0975f2663e68e6e8ae
abidhussain <waniabid645@gmail.com>
2025-02-20 10:26:03 +0530

    rotuer and server.js all apis related for user need to user endpoint to access all user apis in user.router.js file

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,5 +1,17 @@
-const getUser =()=>{
-    return {id:"101",name: "Abid",address : "Ajmer"};
-}
-module.exports = {getUser};
+const express = require('express');
 
+module.exports = (app) => {
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        console.log('User endpoint hit');
+        res.send("Hello User");
+    });
+
+    router.get('/:id',(req,res)=>{
+        res.send(req.params.id);
+    })
+
+
+    app.use('/user', router);
+};
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const port = 3004;
+const port = process.env.PORT || 3004;
 const app = express();
 app.use(express.json());
-const user = require('./routes/users.routes');
-// console.log(user.getUser());
-app.get('/',(req,res)=>{
-  res.send("welcome to express js ");
-})
-app.get('/user',(req,res)=>{
-  res.json({data : user.getUser() , success: true});
-})
-app.listen(port);
+require('./routes/users.routes')(app);
+
+app.get('/', (req, res) => {
+  res.status(200).send({ message: "Welcome to Express JS" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
