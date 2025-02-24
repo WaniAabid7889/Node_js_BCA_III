1d8fbc7bccccfe2a85a69a8a25a3a405a47c5a7f
abidhussain <waniabid645@gmail.com>
2025-02-24 10:17:40 +0530

    today we learn get and put and delete method and also some method in array like some findIndex and push

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -55,17 +55,47 @@ module.exports = (app) => {
   });
 
   router.get("/:id", (req, res) => {
-    let response = result.some((val)=>{
-        if(val.id === parseInt(req.params.id)){
-            res.send(val);
-        }
-    })
+    let response = result.some((val) => {
+      if (val.id === parseInt(req.params.id)) {
+        res.send(val);
+      }
+    });
   });
 
-  router.post('/insert',(req,res)=>{
-    
+  router.post("/insert", (req, res) => {
+    // res.send(req.body);
+    result.push(req.body);
+    res.send(result);
   });
 
+  // router.get('/allUser',(req,res)=>{
+  //   console.log('hello user')
+  //   // res.json({data : result});
+  //   res.send("all users");
+  // })
+
+  router.put("/update/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    //res.send(result);
+    let response = result.some((val) => {
+      if (val.id === parseInt(req.params.id)) {
+        val.username = req.body.username;
+        res.send(val);
+      }
+    });
+  });
+
+ router.delete('/:id',(req,res)=>{
+  const id = parseInt(req.params.id);
+  const index = result.findIndex((val) => val.id === id);
+  if (index !== -1) {
+    result.splice(index, 1);
+    res.send(result);
+  } else {
+    res.status(404).send({ message: 'User not found' });
+  }
+})
 
   app.use("/user", router);
 };
